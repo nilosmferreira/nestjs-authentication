95e98affbcc84b2d2c37f5e89ba6a6e630690a4d
Nilo Ferreira <nsmf.gnf@gmail.com>
2021-09-16 07:52:06 -0300

    if applied, feature jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/IUser';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) { }
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService) { }
   async validateUser(username: string, password: string): Promise<any>{
     const user = await this.userService.findOne(username);
     
@@ -11,5 +15,11 @@ export class AuthService {
       return  user;
     }
     return undefined;
+  }
+  login(user: User) {
+    const payload = { username: user.username, name: user.name, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload)
+    }
   }
 }
